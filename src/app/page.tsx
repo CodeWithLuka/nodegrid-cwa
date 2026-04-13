822496c7c8e2c6f73753cdebd43e20c5149d3a78
CodeWithLuka <lukasuave.nextai@gmail.com>
2026-04-13 14:04:03 +0200

    feat 02: Setup NeoN DB & Prisma ORM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-2xl"> Think, Different</h1>
       <Button>Apple</Button>
+      <p>{JSON.stringify(users)}</p>
     </div>
   );
 }
