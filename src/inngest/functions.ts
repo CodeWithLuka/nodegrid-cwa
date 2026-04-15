cc9af822d477d03c55dd1d6d96e59d5f479460be
CodeWithLuka <lukasuave.nextai@gmail.com>
2026-04-15 05:16:00 +0200

    feat 08: Setup Sentry For Error Tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -19,33 +19,17 @@ export const processTask = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant",
         prompt: "What is aerodynamics?.",
-      },
-    );
-
-    const { steps: openAiSteps } = await step.ai.wrap(
-      "open-ai-generate-text",
-      generateText,
-      {
-        model: openAi("gpt-4o"),
-        system: "You are a helpful assistant",
-        prompt: "What is aerodynamics?.",
-      },
-    );
-
-    const { steps: anthropicSteps } = await step.ai.wrap(
-      "anthropic-generate-text",
-      generateText,
-      {
-        model: anthropic("claude-opus-4-0"),
-        system: "You are a helpful assistant",
-        prompt: "What is aerodynamics?.",
+        experimental_telemetry: {
+          isEnabled: true,
+          functionId: "gemini_agent",
+          recordInputs: true,
+          recordOutputs: true,
+        },
       },
     );
 
     return {
       geminiSteps,
-      openAiSteps,
-      anthropicSteps,
     };
   },
 );
